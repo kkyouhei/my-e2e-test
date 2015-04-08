054d100159332a5c0bd29352fba3267c904e424d
kkyouhei <karino@basicinc.jp>
2015-04-08 18:13:13 +0900

    ua変更

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,19 +7,22 @@ require 'capybara/rspec'
 require 'capybara/poltergeist'
 require 'turnip'
 require 'turnip/capybara'
+require 'selenium/webdriver'
 
 RSpec.configure do |config|
-
 end
 
 Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+  profile = Selenium::WebDriver::Firefox::Profile.new
+  ua = 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A403 Safari/8536.25'
+  profile["general.useragent.override"] = ua
+  opts = { browser: :firefox, profile: profile }
+  Capybara::Selenium::Driver.new app, opts
 end
 
 if (ENV['TARGET'].nil? || ENV['TARGET'] != 'poltergeist')
   Capybara.default_driver = :selenium
 else
-  require 'capybara/poltergeist'
   Capybara.default_driver    = :poltergeist
   Capybara.javascript_driver = :poltergeist
 end
